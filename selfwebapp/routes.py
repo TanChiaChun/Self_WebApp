8791402d046b070f9077bf4bff5a58270224c9d2
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-02-16 13:34:58 +0800

    Add proddash.html + use Bootstrap + others
    -Change HTML title
    -Add favicon for desktop drowsers & iOS
    -Add css for dark mode

--- selfwebapp/routes.py
+++ selfwebapp/routes.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, url_for
 from selfwebapp import app
 
 @app.route("/")
 def home():
     return render_template("home.html")
+
+@app.route("/proddash")
+def proddash():
+    return render_template("proddash.html")
