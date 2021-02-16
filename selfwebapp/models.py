47879b4ed6bb16cc72fbd6c73da219464477580a
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-02-16 16:48:16 +0800

    Add login + DB connectivity + others
    -Create User class
    -Restrict pages & components with login required
    -Introduce Flask flash message
    -Change HTML title

---
+++ selfwebapp/models.py
@@ -0,0 +1,20 @@
+from selfwebapp import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+
+    def __repr__(self):
+        return f"User({self.username})"
+
+def init_db():
+    db.create_all()
+    user1 = User(username="hydrochun", password="password")
+    db.session.add(user1)
+    db.session.commit()
--- selfwebapp/routes.py
+++ selfwebapp/routes.py
@@ -1,14 +1,36 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request, redirect, flash
 from selfwebapp import app
+from selfwebapp.models import User
+from flask_login import login_user, current_user, logout_user, login_required
 
 @app.route("/")
+@login_required
 def home():
     return render_template("home.html")
 
 @app.route("/proddash")
+@login_required
 def proddash():
     return render_template("proddash.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for("home"))
+    if request.method == "POST":
+        username = request.form.get("inputUsername")
+        password = request.form.get("inputPassword")
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            flash("Login successful", category="success")
+            return redirect(url_for("home"))
+        else:
+            flash("Login unsuccessful", category="danger")
     return render_template("login.html")
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("login"))
