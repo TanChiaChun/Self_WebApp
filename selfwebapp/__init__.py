62f414c2d17df10421cbe4647dbbd8c3fe411deb
Tan Chia Chun <54241577+TanChiaChun@users.noreply.github.com>
2021-02-16 21:39:52 +0800

    Change Flask secret to env_var

--- selfwebapp/__init__.py
+++ selfwebapp/__init__.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+import os
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "key"
+app.config["SECRET_KEY"] = os.getenv("Flask_Secret_1")
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///site.db"
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
